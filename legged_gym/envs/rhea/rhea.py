a89094bbe37bb4c4fd338f2c994b6b0179a2a7ae
Gabrael Levine <gabrael@stanford.edu>
2023-09-09 00:20:14 +0000

    Add orientation randomization

--- legged_gym/envs/rhea/rhea.py
+++ legged_gym/envs/rhea/rhea.py
@@ -204,3 +204,26 @@ class Rhea(LeggedRobot):
 
     #     return actuator_torques
 
+    def _reset_root_states(self, env_ids):
+        """ Resets ROOT states position and velocities of selected environmments
+            Sets base position based on the curriculum
+            Selects randomized base velocities within -0.5:0.5 [m/s, rad/s]
+        Args:
+            env_ids (List[int]): Environemnt ids
+        """
+        # base position
+        if self.custom_origins:
+            self.root_states[env_ids] = self.base_init_state
+            self.root_states[env_ids, :3] += self.env_origins[env_ids]
+            self.root_states[env_ids, :2] += torch_rand_float(-1., 1., (len(env_ids), 2), device=self.device) # xy position within 1m of the center
+        else:
+            self.root_states[env_ids] = self.base_init_state
+            self.root_states[env_ids, :3] += self.env_origins[env_ids]
+        # base orientation
+        self.root_states[env_ids, 3:7] = quat_from_angle_axis(torch_rand_float(-0.52, 0.52, (1, len(env_ids)), device=self.device), torch_rand_float(-1., 1., (len(env_ids), 3), device=self.device))[0]
+        # base velocities
+        self.root_states[env_ids, 7:13] = torch_rand_float(-0.5, 0.5, (len(env_ids), 6), device=self.device) # [7:10]: lin vel, [10:13]: ang vel
+        env_ids_int32 = env_ids.to(dtype=torch.int32)
+        self.gym.set_actor_root_state_tensor_indexed(self.sim,
+                                                     gymtorch.unwrap_tensor(self.root_states),
+                                                     gymtorch.unwrap_tensor(env_ids_int32), len(env_ids_int32))
